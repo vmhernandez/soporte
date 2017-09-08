2e44e83451b3699376e397c472f5c8f92237640a
Víctor Manuel Hernández Vergara <vmhernandez@ing.ucsc.cl>
2017-09-08 17:17:30 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+ include "funciones/fconsulta.php";
+?>
+  <!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Registro de equipos</title>
+  </head>
+
+  <body>
+    <?php
+          test();
+        ?>
+  </body>
+
+</html>
